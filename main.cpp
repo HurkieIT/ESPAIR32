70f8aef39dd6bad897b839de0b433390515fc09d
Jens van den Hurk <jensvandenhurk@outlook.com>
2026-02-03 10:12:01 +0100

    ESPAIR32 opstelling gemaakt

---
+++ main.cpp
@@ -0,0 +1,9 @@
+void setup() {
+  // Draait ÉÉN keer bij opstarten
+  // Hier zet je initialisatie: Serial.begin(), WiFi.begin(), pin modes, etc.
+}
+
+void loop() {
+  // Herhaalt ONEINDIG (dit is je main loop)
+  // Hier komt je hoofdcode die blijft draaien
+}
