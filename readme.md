f1b4aa65157ca3378dcba3beb71e387094a98d1d
Jens van den Hurk <jensvandenhurk@outlook.com>
2026-02-03 11:20:48 +0100

    Even opgeschoont en professioneler gemaakt

---
+++ readme.md
@@ -0,0 +1,3 @@
+**Wat is dit project**
+Dit project is een kleine wifi scanner gebaseerd op een ESP32-WROOM
+
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // Draait ÉÉN keer bij opstarten
-  // Hier zet je initialisatie: Serial.begin(), WiFi.begin(), pin modes, etc.
-}
-
-void loop() {
-  // Herhaalt ONEINDIG (dit is je main loop)
-  // Hier komt je hoofdcode die blijft draaien
-}
